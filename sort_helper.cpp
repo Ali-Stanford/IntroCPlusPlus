709ccc209d68e03ac33c41e8c96f1ade22f6bc42
Ali-Stanford <58951525+Ali-Stanford@users.noreply.github.com>
2024-05-27 16:31:38 -0700

    Add files via upload

---
+++ sort_helper.cpp
@@ -0,0 +1,16 @@
+void swap(Character& a, Character& b) {
+
+    Character temp = a;
+    a = b;
+    b = temp;
+}
+
+void sortVector(vector<Character>& vec) {
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3 - i; ++j) {
+            if (vec[j].getPosition() > vec[j + 1].getPosition()) {
+                swap(vec[j], vec[j + 1]);
+            }
+        }
+    }
+}

24438b0f2faf3739c0cdb26ef6da5701273cc44f
Ali-Stanford <58951525+Ali-Stanford@users.noreply.github.com>
2024-05-27 16:29:11 -0700

    Update helper.cpp

--- helper.cpp
+++ helper.cpp
@@ -6,17 +6,17 @@ void generateNarrativeOfNoChampion() {
 
 void generateNarrativeOfChampion(Charachter A) {
 
-    cout << "In the vast, sunbaked expanse of the Wild West, a solitary figure emerges victorious against all odds. " + A.getName() + " standing triumphant at the position of " << A.getPosition() << ", clutches the elusive treasure. With " << A.getColdCoin() << " gold coins jingling in their pocket, " << A.getSilverCoin() << " silver coins shimmering in the sunlight, and " << A.getBronzeCoin() << " bronze coins bearing the weight of their journey, " << A.getName() << " stands resolute, their trusty " << A.getGunType() << " gun by their side. This moment marks not just the end of a treacherous path but the beginning of a legend, as " << A.getName() << " etches their name in the annals of the West as the undisputed winner of the treasure." << endl;
+    cout << "In the vast, sunbaked expanse of the Wild West, a solitary figure emerges victorious against all odds. " + A.getName() + " standing triumphant at the position of " << A.getPosition() << ", clutches the elusive treasure. With " << A.getGoldCoin() << " gold coins jingling in their pocket, " << A.getSilverCoin() << " silver coins shimmering in the sunlight, and " << A.getBronzeCoin() << " bronze coins bearing the weight of their journey, " << A.getName() << " stands resolute, their trusty " << A.getGunType() << " gun by their side. This moment marks not just the end of a treacherous path but the beginning of a legend, as " << A.getName() << " etches their name in the annals of the West as the undisputed winner of the treasure." << endl;
 }
 
 void generateNarrativeOfDuel(Charachter Victim, Charachter Murderer) {
 
     cout << "In the rugged landscape of this Western movie, a dramatic duel unfolds. "
         << Victim.getName() << ", armed with " << Victim.getGunType() << ", and holding "
-        << Victim.getColdCoin() << " gold coins, " << Victim.getSilverCoin()
+        << Victim.getGoldCoin() << " gold coins, " << Victim.getSilverCoin()
         << " silver coins, and " << Victim.getBronzeCoin() << " bronze coins, faces a grim fate at the hands of "
-        << Murderer.getName() << ", wielding the superior " << Murderer.gun_type << ". At this pivotal moment, "
-        << Murderer.getName() << " stands with " << Murderer.getColdCoin() << " gold coins, "
+        << Murderer.getName() << ", wielding the superior " << Murderer.getGunType() << ". At this pivotal moment, "
+        << Murderer.getName() << " stands with " << Murderer.getGoldCoin() << " gold coins, "
         << Murderer.getSilverCoin() << " silver coins, and " << Murderer.getBronzeCoin()
         << " bronze coins. The aftermath of this duel paints a vivid picture of the harsh West: "
         << Victim.getName() << " falls at step " << Victim.getPosition() << ", their journey ending amidst the dust and echoes of gunshots, while "
